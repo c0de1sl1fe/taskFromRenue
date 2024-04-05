200b5a7918a3c90ddfae30724fa1550bbd3f218e
c0de1sl1fe <everydaysmok@gmail.com>
2024-04-05 22:27:37 +0400

    add parse from cmd

--- src/main/java/com/ykic/Main.java
+++ src/main/java/com/ykic/Main.java
@@ -1,5 +1,8 @@
 package com.ykic;
 
+import org.apache.commons.cli.*;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -19,10 +22,52 @@ public class Main {
 //        List<Integer> foundIndexes = trie.search("apple");
 //        System.out.println(foundIndexes);
 //
-        ReadTXT readTXT = new ReadTXT("D:\\\\git\\\\courseGeekBrains\\\\src\\\\main\\\\resources\\\\test.txt");
-        readTXT.readData();
-        System.out.println(readTXT.getData());
 
+
+//        ReadTXT readTXT = new ReadTXT("D:\\\\git\\\\courseGeekBrains\\\\src\\\\main\\\\resources\\\\test.txt");
+//        readTXT.readData();
+//        System.out.println(readTXT.getData());
+
+        Options options = new Options();
+
+        Option dataOption = new Option("d", "data", true, "path to data");
+        dataOption.setRequired(true);
+        options.addOption(dataOption);
+
+        Option indexOption = new Option("i", "indexed-column-id", true, "column index");
+        indexOption.setRequired(true);
+        options.addOption(indexOption);
+
+        Option inputFileOption = new Option("in", "input-file", true, "path to input file");
+        inputFileOption.setRequired(true);
+        options.addOption(inputFileOption);
+
+        Option outputFileOption = new Option("out", "output-file", true, "path to output file");
+        outputFileOption.setRequired(true);
+        options.addOption(outputFileOption);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+            System.exit(-1);
+            return;
+        }
+
+        String dataPath = cmd.getOptionValue("data");
+        String indexedColumnId = cmd.getOptionValue("indexed-column-id");
+        String inputFilePath = cmd.getOptionValue("input-file");
+        String outputFilePath = cmd.getOptionValue("output-file");
+
+        System.out.println("Путь к данным: " + dataPath);
+        System.out.println("Индексированный ID колонки: " + indexedColumnId);
+        System.out.println("Путь к входному файлу: " + inputFilePath);
+        System.out.println("Путь к выходному файлу: " + outputFilePath);
     }
 }
 
